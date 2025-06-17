ac29b59017022057bc88b923fc5da3fc9980aa7f
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-06-17 23:58:07 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 if isAuthorized {
-                    List {//List of all workouts there 
+                    List {//List of all workouts there
                         Section(header: Text("Workouts---there")) {
                             ForEach(workouts, id: \.uuid) { workout in
                                 VStack(alignment: .leading) {
@@ -26,7 +26,7 @@ struct ContentView: View {
                         }
 
                         Section {
-                            Text("Total Calories Burned per body in kcal: \(Int(totalCalories)) kcal")
+                            Text("Total Calories Burned per body in kcal--: \(Int(totalCalories)) kcal")
                                 .font(.title2)
                                 .bold()
                         }
