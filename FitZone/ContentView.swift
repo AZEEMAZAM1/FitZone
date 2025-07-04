a39b671e77ed63d92058d5a4077f198061f02e41
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-07-04 17:18:37 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -94,7 +94,7 @@ extension HKWorkoutActivityType {
     var name: String {
         switch self {
         case .running: return "Running-"
-        case .cycling: return "Cycling-"
+        case .cycling: return "-Cycling-"
         case .walking: return "-Walking-"
         case .functionalStrengthTraining: return "Strength_Training-"
         case .traditionalStrengthTraining: return "Weight Lifting-"
