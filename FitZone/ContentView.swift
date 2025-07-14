56517539afbddf0894b7838a24499239f2535537
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-07-14 22:41:54 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             VStack {
                 if isAuthorized {
                     List {//List of all workouts there
-                        Section(header: Text("-Workout's---there-")) {
+                        Section(header: Text("--Workout's---there-")) {
                             ForEach(workouts, id: \.uuid) { workout in
                                 VStack(alignment: .leading) {
                                     Text(workout.workoutActivityType.name)
