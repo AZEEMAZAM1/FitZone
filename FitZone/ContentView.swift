8b947b7495d9e46e21078bfd335d60221aa2401e
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-07-22 09:10:05 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                     .padding()
                 }
             }
-            .navigationTitle("-F-i-t-Z-o-n-e_T-racker--")
+            .navigationTitle("-F-i-t-Z-o-n-e_T-r-acker--")
         }
         .onAppear {
             if HKHealthStore.isHealthDataAvailable() {
