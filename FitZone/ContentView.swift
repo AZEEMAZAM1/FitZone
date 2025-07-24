1f7c33379bde03ceab1ead7ec399b02dd2c67848
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-07-24 17:33:55 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
                 isAuthorized = true
                 fetchWorkouts()
             } else {
-                print("-H-e-a-l-t-h-Kit-au-t-h-or-iza-tion-failed (due-to-low-blood pres-sure--ds)  : \(error?.localizedDescription ?? "-Unknown-error-")")
+                print("-H-e-a-l-t-h-K-it-au-t-h-or-iza-tion-failed (due-to-low-blood pres-sure--ds)  : \(error?.localizedDescription ?? "-Unknown-error-")")
             }
         }
     }
