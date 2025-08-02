8505131e78e1f1de81e04b2c6c55f5d0f7387ddd
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-08-02 09:42:56 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -32,7 +32,7 @@ struct ContentView: View {
                         }
                     }
                 } else {
-                    Button("-Au-thori__ze HealthK_it__") {
+                    Button("-A-u-thori__ze HealthK_it__") {
                         requestHealthKitPermission()
                     }
                     .padding()
