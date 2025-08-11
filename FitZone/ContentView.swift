111077acb8529158eec9daac6d82ab18f1ac2bf7
AZEEMAZAM1 <MUHAMMADAZEEM3010@GMAIL.COM>
2025-08-11 17:17:03 +0100

    Update: added new features

--- FitZone/ContentView.swift
+++ FitZone/ContentView.swift
@@ -103,7 +103,7 @@ extension HKWorkoutActivityType {
         case .running: return "🏃 Running, singing"
         case .cycling: return "🚴 Cycling, eating"
         case .walking: return "🚶 Walking, dancing"
-        case .functionalStrengthTraining: return "🏋️ Functional Strength and muscular strength"
+        case .functionalStrengthTraining: return "🏋️ Functional and sesonal Strength and muscular strength"
         case .traditionalStrengthTraining: return "💪 Weight Lifting"
         case .elliptical: return "🌀 Elliptical"
         case .swimming: return "🏊 Swimming"
